99d47a23d0fb4435ace2760d6eeb6836a14726bb
Amos Sejour <sejouramos17@gmail.com>
2022-07-21 18:32:39 -0400

    Update ip_auto at true

--- provider.tf
+++ provider.tf
@@ -33,6 +33,7 @@ resource "aws_subnet" "subnet1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
   availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "Main"
@@ -45,6 +46,7 @@ resource "aws_subnet" "subnet2" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.3.0/24"
   availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
 }
 
 #create Route Table for subnet 1
