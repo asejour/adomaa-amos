0f3270040b03a9d1de8ce64bbbfbaee2eb79ad3c
asejour <78275066+asejour@users.noreply.github.com>
2022-07-29 10:09:37 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -139,8 +139,8 @@ resource "aws_eks_node_group" "eks_ng_public" {
   }
 
   scaling_config {
-    desired_size = 1
-    min_size     = 1    
+    desired_size = 2
+    min_size     = 2    
     max_size     = 2
   }
 
